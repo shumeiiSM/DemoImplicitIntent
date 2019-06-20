4e4f1fa22c792eaf44e898896a2e5d54a207be48
17010233 <17010233@myrp.edu.sg>
2019-06-20 11:16:40 +0800

    email 2 person

--- app/src/main/java/com/example/a17010233/demoimplicitintent/MainActivity.java
+++ app/src/main/java/com/example/a17010233/demoimplicitintent/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent email = new Intent(Intent.ACTION_SEND);
                 // Put essentials like email address, subject & body text
                 email.putExtra(Intent.EXTRA_EMAIL,
-                        new String[]{"jason_lim@rp.edu.sg"});
+                        new String[]{"jason_lim@rp.edu.sg", "meimei@rp.edu.sg"});
                 email.putExtra(Intent.EXTRA_SUBJECT,
                         "Test Email from C347");
                 email.putExtra(Intent.EXTRA_TEXT,
